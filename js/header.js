36da148290a7f99863fa4301a6ae881722c88f84
gjouet83 <jouetguillaume@gmail.com>
2023-09-06 09:25:29 +0200

    convert react project karine-kinesio to simple html css js

---
+++ js/header.js
@@ -0,0 +1,11 @@
+const handleClickOpen = () => {
+  let open = document.getElementById('nav');
+
+  open.classList.toggle('isOpen');
+};
+
+const handleClickClose = () => {
+  let close = document.getElementById('nav');
+
+  close.classList.toggle('isOpen');
+};
